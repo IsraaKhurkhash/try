3b5d61e40ee50dfa6fd5ec9dbe68b52155874170
IsraaKhurkhash <40401738+IsraaKhurkhash@users.noreply.github.com>
2019-05-28 14:53:30 +0200

    Add files via upload

---
+++ 3D points/3D points/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_points
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point(int _x = 0, int _y = 0, int _z = 0)
+        {
+            X = _x;
+            Y = _y;
+            Z = _z;
+
+        }
+
+        public double caldistance(Point v)
+        {
+
+            double d = Math.Sqrt(Math.Pow(v.X - this.X, 2) + Math.Pow(v.Y - this.Y, 2) + Math.Pow(v.Z - this.Z, 2));
+            return d;
+        }
+    }
+}
---
+++ 3D points/3D points/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3D_points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(2, 2, 3);
+            Point p2 = new Point(1, 2, 1);
+
+            Console.Write(p1.caldistance(p2));
+            Console.Read();
+        }
+    }
+}
